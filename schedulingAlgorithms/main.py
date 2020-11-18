dcb6073502286f99518315a475d562c6ce90c5c8
BuildTools <justinkrasner98@gmail.com>
2020-11-18 17:41:12 -0500

    scheduling algorithms asgnmnt

---
+++ schedulingAlgorithms/main.py
@@ -0,0 +1,11 @@
+# Write a program the implements 3 of the following disk-scheduling algorithms:
+# a. FCFS
+# b. SSTF
+# c. SCAN
+# d. C-SCAN
+# e. LOOK
+# f. C-LOOK
+# Your program will service a disk drive with 5,000 cylinders numbered 0-4,999.
+# The program will generate a random series of 1,000 requests and service them according to three of the
+# algorithms listed above. The program will be passed the initial position of the disk head as a parameter
+# on the command line and report the total amount of head movement by each algorithm.
