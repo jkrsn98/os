b5dee228a4279bb0f9895e8d6f5e0d3ea3272486
BuildTools <justinkrasner98@gmail.com>
2020-11-21 12:11:11 -0500

    added fcfs

--- schedulingAlgorithms/main.py
+++ schedulingAlgorithms/main.py
@@ -9,3 +9,29 @@
 # The program will generate a random series of 1,000 requests and service them according to three of the
 # algorithms listed above. The program will be passed the initial position of the disk head as a parameter
 # on the command line and report the total amount of head movement by each algorithm.
+
+import sys
+import math
+from numpy import random
+
+arr = []
+
+head = int(sys.argv[1])
+
+i = 0
+while i != 1000:
+    r = random.randint(0, 5000)
+    arr.append(r)
+    i = i + 1
+
+
+def FCFS(arr, head):
+    count = 0
+    for i in range(len(arr)):
+        cur = arr[i]
+        dist = abs(cur - head)
+        count = dist + count
+        head = cur
+    print('\nFCFS:\n number of movements: %s' % (count))
+    
+FCFS(arr, head)
